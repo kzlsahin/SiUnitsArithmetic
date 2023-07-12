23b46d0c797ef230d46ff08ddf890c7e784562be
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-07-12 18:47:23 +0300

    WIP - CompositeUnit is added.

---
+++ src/CompositeUnit.cs
@@ -0,0 +1,32 @@
+﻿using SIUnits.Length;
+using SIUnits.Mass;
+using SIUnits.Time;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIUnits
+{
+    internal sealed class CompositeUnit
+    {
+        readonly MetricLength l_unit;
+        readonly MetricTime t_unit;
+        readonly MetricMass m_unit;
+
+        CompositeUnit() :this(new MetricLength(), new MetricTime(), new MetricMass())
+        {
+
+        }
+        CompositeUnit(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit)
+        {
+            l_unit = lengthUnit;
+            t_unit = timeUnit;
+            m_unit = massUnit;
+        }
+
+        public double Value { get; }
+        public string Symbol { get; }
+
+
+    }
+}
