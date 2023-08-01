8015570ae03d919c307b071518dd2aa67518c16d
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-08-01 09:14:35 +0300

    Unit tests are improved for special units.

--- MetricUnitTests/ArithmeticTests/DerivedUnitTests.cs
+++ MetricUnitTests/ArithmeticTests/DerivedUnitTests.cs
@@ -32,14 +32,25 @@ namespace MetricUnitTests.ArithmeticTests
             der2 = 0.01.km() * 1000.g() / 1.second(2);
             Assert.AreEqual(der1 == der2, true);
 
+           
+        }
+        [TestMethod]
+        public void SpecialUnitTests()
+        {
+            UnitConfig.UnitPrecision = 4;
+
             Newton f1 = (Newton)((1.kg() * 1.m()) / 1.second(2));
             Assert.IsTrue(f1 is Newton);
 
             var speed = 2.km() / 1.hour();
             var acc = speed / 1.hour();
+            var f2 = acc * 80.kg();
+            var e1 = f2 * 500.mm();
+            Joule e2 = (Joule)(f1 * 10.m());
+            Assert.IsTrue(e1 is Joule);
+            Assert.IsTrue(f2 is Newton);
+            Assert.IsTrue(e2 is Joule);
 
-            Joule energy = (Joule)(f1 * 10.m());
-            Assert.IsTrue(energy is Joule);
         }
     }
 }
