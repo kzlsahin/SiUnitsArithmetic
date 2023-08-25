c7969030feb960f7ecf5f2b9d584bc8cd33507f1
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2023-08-25 14:09:00 +0300

    Feature - Ohm and Volt special derived units are added.

---
+++ src/Electric/Ohm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIUnits
+{
+    /// <summary>
+    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2⋅s−3⋅A−2.
+    /// </summary>
+    public sealed class Ohm : DerivedUnit
+    {
+        internal readonly static DerivedDegree refDegree = new DerivedDegree(2, -3, 1, -2);
+
+        Ohm(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit) : base(lengthUnit.m(), timeUnit.second(), massUnit.kg(), Ampere.ScalerOne)
+        {
+        }
+        /// <summary>
+        /// initializes a Ohm type DerivedUnit in kg.m/s2.
+        /// </summary>
+        /// <param name="lengthUnit"></param>
+        /// <param name="timeUnit"></param>
+        /// <param name="massUnit"></param>
+        public static new Ohm New(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit)
+        {
+            return new Ohm(lengthUnit, timeUnit, massUnit);
+        }
+        /// <summary>
+        /// converts a derived unit into Ohm if the derivedunit is in kg⋅m2⋅s−3⋅A−2.
+        /// </summary>
+        /// <param name="derivedUnit"></param>
+        /// <param name="ohm"></param>
+        /// <returns></returns>
+        public bool TryConvert(DerivedUnit derivedUnit, out Ohm ohm)
+        {
+            if (derivedUnit.Degree == Ohm.refDegree)
+            {
+                ohm = new Ohm(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
+                return true;
+            }
+            ohm = null;
+            return false;
+        }
+        /// <summary>
+        /// converts a derived unit into Ohm if the derivedunit is in kg⋅m2⋅s−3⋅A−1.
+        /// </summary>
+        /// <param name="derivedUnit"></param>
+        /// <returns></returns>
+        public Ohm Convert(DerivedUnit derivedUnit)
+        {
+            Ohm ohm;
+            if (derivedUnit.Degree == Ohm.refDegree)
+            {
+                ohm = new Ohm(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
+                return ohm;
+            }
+            ohm = null;
+            return ohm;
+        }
+    }
+}
---
+++ src/Electric/Volt.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIUnits
+{
+    /// <summary>
+    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2⋅s−3⋅A−1.
+    /// </summary>
+    public sealed class Volt : DerivedUnit
+    {
+        internal readonly static DerivedDegree refDegree = new DerivedDegree(2, -3, 1, -1);
+
+        Volt(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit) : base(lengthUnit.m(), timeUnit.second(), massUnit.kg(), Ampere.ScalerOne)
+        {
+        }
+        /// <summary>
+        /// initializes a Volt type DerivedUnit in kg.m/s2.
+        /// </summary>
+        /// <param name="lengthUnit"></param>
+        /// <param name="timeUnit"></param>
+        /// <param name="massUnit"></param>
+        public static new Volt New(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit)
+        {
+            return new Volt(lengthUnit, timeUnit, massUnit);
+        }
+        /// <summary>
+        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2⋅s−3⋅A−1.
+        /// </summary>
+        /// <param name="derivedUnit"></param>
+        /// <param name="volt"></param>
+        /// <returns></returns>
+        public bool TryConvert(DerivedUnit derivedUnit, out Volt volt)
+        {
+            if (derivedUnit.Degree == Volt.refDegree)
+            {
+                volt = new Volt(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
+                return true;
+            }
+            volt = null;
+            return false;
+        }
+        /// <summary>
+        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2⋅s−3⋅A−1.
+        /// </summary>
+        /// <param name="derivedUnit"></param>
+        /// <returns></returns>
+        public Volt Convert(DerivedUnit derivedUnit)
+        {
+            Volt volt;
+            if (derivedUnit.Degree == Volt.refDegree)
+            {
+                volt = new Volt(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
+                return volt;
+            }
+            volt = null;
+            return volt;
+        }
+    }
+}
