eb4ba137925ff30f744497a2cc6a5a8e615d2a01
Mustafa SENTÜRK <deniz_kurdu_1@hotmail.com>
2024-03-11 14:18:58 +0300

    refactor - Ohm and Volt classes are refactored.

--- src/Electric/Ohm.cs
+++ src/Electric/Ohm.cs
@@ -1,31 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace SIUnits
 {
     /// <summary>
-    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2⋅s−3⋅A−2.
+    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2 /s3⋅A2.
     /// </summary>
     public sealed class Ohm : DerivedUnit
     {
-        internal readonly static DerivedDegree refDegree = new DerivedDegree(2, -3, 1, -2);
+        internal readonly static DerivedDegree refDegree = new DerivedDegree(
+            new KeyValuePair<Guid, int>(MetricLength.ID, 2),
+            new KeyValuePair<Guid, int>(MetricTime.ID, -3),
+            new KeyValuePair<Guid, int>(MetricMass.ID, 1),
+            new KeyValuePair<Guid, int>(Ampere.ID, -2)
+            );
 
-        Ohm(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit) : base(lengthUnit.m(), timeUnit.second(), massUnit.kg(), Ampere.ScalerOne)
+        Ohm(params IBasicUnit[] units) : base(units)
         {
         }
         /// <summary>
-        /// initializes a Ohm type DerivedUnit in kg.m/s2.
+        /// initializes a Ohm type DerivedUnit in kg.m/s2.A2.
         /// </summary>
-        /// <param name="lengthUnit"></param>
-        /// <param name="timeUnit"></param>
-        /// <param name="massUnit"></param>
-        public static new Ohm New(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit)
+        internal static new Ohm New(Dictionary<Guid, IBasicUnit> memberUnits)
         {
-            return new Ohm(lengthUnit, timeUnit, massUnit);
+            Ohm ohm;
+            if (
+                memberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                memberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                memberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                memberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+            {
+                ohm = new Ohm((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                return ohm;
+            }
+            return null;
         }
         /// <summary>
-        /// converts a derived unit into Ohm if the derivedunit is in kg⋅m2⋅s−3⋅A−2.
+        /// initializes a Ohm type DerivedUnit in kg.m/s2.A2.
+        /// </summary>
+        public static Ohm New(double value)
+        {
+            var l_unit = (value).m(2);
+            var t_unit = 1.second(-3);
+            var m_unit = 1.kg();
+            var a_unit = 1.A(-2);
+            return new Ohm(l_unit, t_unit, m_unit, a_unit);
+        }
+        
+        /// <summary>
+        /// converts a derived unit into Ohm if the derivedunit is in kg⋅m2/s3⋅A2.
         /// </summary>
         /// <param name="derivedUnit"></param>
         /// <param name="ohm"></param>
@@ -34,8 +60,16 @@ namespace SIUnits
         {
             if (derivedUnit.Degree == Ohm.refDegree)
             {
-                ohm = new Ohm(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
-                return true;
+                if(
+                derivedUnit.MemberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+                {
+                    ohm = new Ohm((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                    return true;
+                }                
             }
             ohm = null;
             return false;
@@ -47,13 +81,19 @@ namespace SIUnits
         /// <returns></returns>
         public Ohm Convert(DerivedUnit derivedUnit)
         {
-            Ohm ohm;
+            Ohm ohm = null;
             if (derivedUnit.Degree == Ohm.refDegree)
             {
-                ohm = new Ohm(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
-                return ohm;
+                if (
+                derivedUnit.MemberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+                {
+                    ohm = new Ohm((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                }
             }
-            ohm = null;
             return ohm;
         }
     }
--- src/Electric/Volt.cs
+++ src/Electric/Volt.cs
@@ -5,27 +5,51 @@ using System.Text;
 namespace SIUnits
 {
     /// <summary>
-    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2⋅s−3⋅A−1.
+    /// A unit of Electric potential difference, Joule per coulomb derived from DerievedUnit in kg⋅m2/s3⋅A.
     /// </summary>
     public sealed class Volt : DerivedUnit
     {
-        internal readonly static DerivedDegree refDegree = new DerivedDegree(2, -3, 1, -1);
-
-        Volt(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit) : base(lengthUnit.m(), timeUnit.second(), massUnit.kg(), Ampere.ScalerOne)
+        internal readonly static DerivedDegree refDegree = new DerivedDegree(
+            new KeyValuePair<Guid, int>(MetricLength.ID, 2),
+            new KeyValuePair<Guid, int>(MetricTime.ID, -3),
+            new KeyValuePair<Guid, int>(MetricMass.ID, 1),
+            new KeyValuePair<Guid, int>(Ampere.ID, -1)
+            );
+        Volt(params IBasicUnit[] units) : base(units)
         {
         }
         /// <summary>
         /// initializes a Volt type DerivedUnit in kg.m/s2.
         /// </summary>
-        /// <param name="lengthUnit"></param>
-        /// <param name="timeUnit"></param>
-        /// <param name="massUnit"></param>
-        public static new Volt New(MetricLength lengthUnit, MetricTime timeUnit, MetricMass massUnit)
+        internal static new Volt New(Dictionary<Guid, IBasicUnit> memberUnits)
         {
-            return new Volt(lengthUnit, timeUnit, massUnit);
+            Volt ohm;
+            if (
+                memberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                memberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                memberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                memberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+            {
+                ohm = new Volt((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                return ohm;
+            }
+            return null;
         }
         /// <summary>
-        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2⋅s−3⋅A−1.
+        /// initializes a Volt type DerivedUnit in kg.m/s2.A
+        /// </summary>
+        public static Volt New(double value)
+        {
+            var l_unit = (value).m(2);
+            var t_unit = 1.second(-3);
+            var m_unit = 1.kg();
+            var a_unit = 1.A();
+            return new Volt(l_unit, t_unit, m_unit, a_unit);
+        }
+
+        /// <summary>
+        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2/s3⋅A.
         /// </summary>
         /// <param name="derivedUnit"></param>
         /// <param name="volt"></param>
@@ -34,26 +58,40 @@ namespace SIUnits
         {
             if (derivedUnit.Degree == Volt.refDegree)
             {
-                volt = new Volt(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
-                return true;
+                if (
+                derivedUnit.MemberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+                {
+                    volt = new Volt((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                    return true;
+                }
             }
             volt = null;
             return false;
         }
         /// <summary>
-        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2⋅s−3⋅A−1.
+        /// converts a derived unit into Volt if the derivedunit is in kg⋅m2s−3⋅A−1.
         /// </summary>
         /// <param name="derivedUnit"></param>
         /// <returns></returns>
         public Volt Convert(DerivedUnit derivedUnit)
         {
-            Volt volt;
+            Volt volt = null;
             if (derivedUnit.Degree == Volt.refDegree)
             {
-                volt = new Volt(derivedUnit.l_unit, derivedUnit.t_unit, derivedUnit.m_unit);
-                return volt;
+                if (
+                derivedUnit.MemberUnits.TryGetValue(MetricLength.ID, out IBasicUnit l_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricTime.ID, out IBasicUnit t_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(MetricMass.ID, out IBasicUnit m_unit) &&
+                derivedUnit.MemberUnits.TryGetValue(Ampere.ID, out IBasicUnit a_unit)
+                )
+                {
+                    volt = new Volt((MetricLength)l_unit, (MetricTime)t_unit, (MetricMass)m_unit, (Ampere)a_unit);
+                }
             }
-            volt = null;
             return volt;
         }
     }
